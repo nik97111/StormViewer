85eb3d0b89f424ec290a8bffa9aa4a11c6a02460
nik <nik971@gmail.com>
2021-02-22 20:29:39 +0300

    Initial commit

---
+++ Project1-1/DetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.swift
+//  Project1-1
+//
+//  Created by nik on 20.01.2021.
+//
+
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    var selectedPictureNumber = 0
+    var totalPictures = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        title = "This image is \(selectedPictureNumber) of \(totalPictures)"
+        
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    
+}
